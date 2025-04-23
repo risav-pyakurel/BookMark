d4e3bb4d99c5fb54481fba7ed3825b0c5de99829
risav-pyakurel <pyakurel.risav@gmail.com>
2025-04-23 09:51:46 +0545

    authentication backend

--- account/views.py
+++ account/views.py
@@ -1,10 +1,17 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from account.forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from account.models import Profile
+
+from .forms import (
+    LoginForm,
+    ProfileEditForm,
+    UserEditForm,
+    UserRegistrationForm,
+)
+from .models import Profile
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -14,9 +21,8 @@ def user_login(request):
             user = authenticate(
                 request,
                 username=cd['username'],
-                password=cd['password']
+                password=cd['password'],
             )
-            
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -27,8 +33,8 @@ def user_login(request):
                 return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-
     return render(request, 'account/login.html', {'form': form})
+
 
 @login_required
 def dashboard(request):
@@ -38,22 +44,23 @@ def dashboard(request):
         {'section': 'dashboard'}
     )
 
+
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-           
+            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-          
-            new_user.set_password(
-                user_form.cleaned_data['password']
-            )
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
             new_user.save()
+            # Create the user profile
             Profile.objects.create(user=new_user)
             return render(
                 request,
                 'account/register_done.html',
-                {'new_user': new_user}
+                {'new_user': new_user},
             )
     else:
         user_form = UserRegistrationForm()
@@ -62,6 +69,7 @@ def register(request):
         'account/register.html',
         {'user_form': user_form}
     )
+
 
 @login_required
 def edit(request):
@@ -73,19 +81,22 @@ def edit(request):
         profile_form = ProfileEditForm(
             instance=request.user.profile,
             data=request.POST,
-            files=request.FILES
+            files=request.FILES,
         )
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(
+                request,
+                'Profile updated successfully'
+            )
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
     return render(
         request,
         'account/edit.html',
-        {
-            'user_form': user_form,
-            'profile_form': profile_form
-        }
+        {'user_form': user_form, 'profile_form': profile_form},
     )
--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
     
 ]
 
@@ -131,3 +132,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS =[
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+    
+    
